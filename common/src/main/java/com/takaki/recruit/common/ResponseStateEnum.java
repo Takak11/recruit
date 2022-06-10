989024827ee93442042e2537de6b501fb1214157
Takaki <87449034@qq.com>
2022-06-10 09:51:47 +0800

    add restful result

---
+++ common/src/main/java/com/takaki/recruit/common/ResponseStateEnum.java
@@ -0,0 +1,49 @@
+package com.takaki.recruit.common;
+
+import java.util.Arrays;
+
+/**
+ * @author Takaki
+ * @date 2022/6/10
+ *
+ */
+public enum ResponseStateEnum {
+    /**
+     * 见各枚举状态message字段
+     */
+    SUCCESS(200, "成功"),
+    ILLEGAL_ARGUMENT(400, "参数错误"),
+    UNAUTHORIZED(401, "未登录/授权的操作"),
+    NOT_FOUND(404, "服务不存在"),
+    INTERNAL_SERVER_ERROR(500, "服务器内部错误");
+
+    private final Integer code;
+    private final String message;
+
+    ResponseStateEnum(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Integer getCodeByName(String name) {
+
+        return Arrays.stream(ResponseStateEnum.values())
+                .filter(response -> response.getMessage().equals(name))
+                .findFirst()
+                .map(ResponseStateEnum::getCode)
+                .orElse(null);
+    }
+
+    @Override
+    public String toString() {
+        return this.name();
+    }
+}
--- recruit-server/src/main/java/com/takaki/recruit/config/MybatisPlusConfig.java
+++ recruit-server/src/main/java/com/takaki/recruit/config/MybatisPlusConfig.java
@@ -1,13 +1,17 @@
 package com.takaki.recruit.config;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author Takaki
  * @date 2022/6/9
  */
 @Configuration
+@EnableTransactionManagement
+@MapperScan(basePackages = {"com.takaki.**.mapper"})
 public class MybatisPlusConfig {
-
 
 }
